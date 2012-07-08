6d1e1c908505e2f674390dda7e573b5d444c079b
sukso96100 <sukso96100@gmail.com>
2012-07-08 12:50:01 +0900

    addtional commit | No functional differences

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 4 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 PREEMPT Fri Jul 6 18:37:37 KST 2012"
+#define LINUX_COMPILE_TIME "18:37:38"
+#define LINUX_COMPILE_BY "youngbindev"
+#define LINUX_COMPILE_HOST "youngbindev-VirtualBox"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
