b2609cd3d2e0656e7b818778c45b361818fcef20
sukso96100 <sukso96100@gmail.com>
2012-07-23 12:22:03 +0900

    Automatically generated/edited files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 10 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Sun Jul 15 20:11:10 KST 2012"
-#define LINUX_COMPILE_TIME "20:11:10"
+#define UTS_VERSION "#10 PREEMPT Mon Jul 23 11:17:43 KST 2012"
+#define LINUX_COMPILE_TIME "11:17:43"
 #define LINUX_COMPILE_BY "youngbindev"
 #define LINUX_COMPILE_HOST "youngbindev-VirtualBox"
 #define LINUX_COMPILE_DOMAIN "(none)"
